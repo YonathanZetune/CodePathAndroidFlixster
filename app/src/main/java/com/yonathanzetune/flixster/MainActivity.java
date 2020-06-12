ac5ac8a84fe2f30a3fbbc11e8f36eb15a42f5614
YonathanZetune <yonathan@zetune.com>
2020-06-12 16:32:48 -0500

    episode 2 finished

--- app/src/main/java/com/yonathanzetune/flixster/MainActivity.java
+++ app/src/main/java/com/yonathanzetune/flixster/MainActivity.java
@@ -3,12 +3,57 @@ package com.yonathanzetune.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.yonathanzetune.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d("JSONRES", json.toString());
+                JSONObject mJson = json.jsonObject;
+                try {
+                    JSONArray results = mJson.getJSONArray("results");
+                    movies = Movie.fromJsonArray(results);
+                    Log.d("MOVIES", movies.toString());
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSON exception getting results", e);
+
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+
     }
 }
